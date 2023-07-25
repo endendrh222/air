6908fc32593acccd05295389d3d16cef242857c3
Lee-hye-won <hewon112311@gmail.com>
2023-07-25 12:59:20 +0900

    07-25

--- src/main/java/com/air/repository/AirClassRepository.java
+++ src/main/java/com/air/repository/AirClassRepository.java
@@ -1,10 +1,8 @@
 package com.air.repository;
 
-import java.util.List;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.air.entity.AirClass;
 
